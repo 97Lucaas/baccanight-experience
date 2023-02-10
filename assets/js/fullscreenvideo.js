37d0c58b84f597b643cc788444224cf32711c98d
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2023-02-10 14:49:43 +0100

    autoplay finish!!!!

--- assets/js/fullscreenvideo.js
+++ assets/js/fullscreenvideo.js
@@ -1,10 +1,25 @@
-
+var elem = document.getElementById("vid");
+elem.pause();
 
 function Video(){
-    var elem = document.getElementById("vid");
     if (elem.requestFullscreen) {
     elem.classList.remove("removed");
     elem.requestFullscreen();
     elem.play();
     }
 }
+
+addEventListener('fullscreenchange', () => { 
+
+    if (!document.fullscreenElement) {
+        elem.classList.add("removed");
+        elem.pause();
+    }
+
+ });
+
+ elem.addEventListener('ended', () => { 
+
+    window.location.replace("../../pages/clues/clue1.html");
+
+ });
